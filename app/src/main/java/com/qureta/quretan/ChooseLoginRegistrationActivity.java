ef052c7f08c8a1d10cc6ee79c72b93c03740ee48
GabrielShaukan <gabrielshaukan@yahoo.com>
2019-03-24 16:19:45 +0100

    Added choose login or registration activity

---
+++ app/src/main/java/com/qureta/quretan/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,38 @@
+package com.qureta.quretan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+
+        mLogin = findViewById(R.id.login);
+        mRegister = findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
